e3b704d3e1b3ba495ca4a93c1f1d889caea7217c
royvroksi <royvroksi@gmail.com>
2026-01-22 22:56:47 +0530

    Fix ESLint flat config override

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,11 @@ const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      "react/no-unescaped-entities": "off"
+    },
   },
 ]);
